516a67bd936eeaf2abdd4faa2749a79d838befcf
rishabhtiwarics <rishabhtiwarics@gmail.com>
2026-09-23 12:46:28 +0530

    "

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
     
     if (track) {
       let offset = 0;
-      const speed = 0.15;
+      const speed = 0.55;
       const tick = () => {
         offset += speed;
         const resetPoint = track.scrollWidth / 2;
